fc8b73892feb79563436f758a1b2265465e0c95e
yooksi <yooksi@zoho.com>
2019-04-14 09:16:01 +0200

    Add MWSE updater to startup script

--- MTE-Updater.bat
+++ MTE-Updater.bat
@@ -1,3 +1,9 @@
 @echo off
-java -jar MTE-Updater.jar
+echo.
+REM Try to update MWSE first
+if exist MWSE-Update.exe (
+	echo Updating MWSE build...
+	start /wait MWSE-Update.exe
+)
+	java -jar MTE-Updater.jar
 Pause

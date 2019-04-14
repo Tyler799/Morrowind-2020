c8636dfe37c040dde26250b446bfc020564a66bc
yooksi <yooksi@zoho.com>
2019-04-14 09:18:27 +0200

    Add Java check to startup script

--- MTE-Updater.bat
+++ MTE-Updater.bat
@@ -5,5 +5,19 @@ if exist MWSE-Update.exe (
 	echo Updating MWSE build...
 	start /wait MWSE-Update.exe
 )
+REM Check if an appropriate version of Java is installed
+echo Checking your Java version...
+PATH %PATH%;%JAVA_HOME%\bin\
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j%%k"
+if %jver% lss 18 ( REM Java 1.8
+    echo.
+	echo ^|========================================^|
+	echo ^|= Missing or incorrect version of Java =^|
+	echo ^|========================================^|
+	echo ------------------------------------------ & echo.
+	echo I am sorry to inform you that you do not have the correct version of Java installed, please install Java SE Runtime Environment 8u202. You can get that version from the official Java website. The URL has been copied to your clipboard, just paste it into your browser search bar and press enter.
+	echo https://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html| clip
+	goto eof
+)
 	java -jar MTE-Updater.jar
 Pause

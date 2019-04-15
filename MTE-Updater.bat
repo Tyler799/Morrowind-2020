618be60b1637ebb2c855416d694e9b5b8224f73b
yooksi <yooksi@zoho.com>
2019-04-15 10:57:59 +0200

    Add custom code to updater script

--- MTE-Updater.bat
+++ MTE-Updater.bat
@@ -12,6 +12,7 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
+set JAVA_VERSION=18
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and MTE_UPDATER_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
@@ -35,7 +36,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto init
+if exist "%JAVA_EXE%" goto checkJavaVersion
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -44,6 +45,29 @@ echo Please set the JAVA_HOME variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
+
+:checkJavaVersion
+@REM Check if an appropriate version of Java is installed
+if not defined JAVA_VERSION goto init
+
+echo Checking your Java version...
+PATH %PATH%;%JAVA_HOME%\bin\
+for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -fullversion 2^>^&1') do set "jver=%%j%%k"
+if %jver% lss %JAVA_VERSION% (
+    echo.
+	echo ERROR: Incorrect version of Java ^(%jver:~0,1%.%jver:~-1%^)
+	echo.
+	echo You do not have the correct version of Java installed
+	echo Please install Java SE Runtime Environment 8u202 ^(Java 8^)
+	echo which you can download from the official Java website
+	echo.
+	echo The URL has been copied to your clipboard
+	echo just paste it into your browser search bar and press enter
+	echo https://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html| clip
+
+	goto fail
+)
+goto init
 
 :init
 @rem Get command-line arguments, handling Windows variants
@@ -61,12 +85,18 @@ if "x%~1" == "x" goto execute
 set CMD_LINE_ARGS=%*
 
 :execute
+@REM Try to update MWSE first
+if exist MWSE-Update.exe (
+	echo Attempting to update MWSE build...
+	start /wait MWSE-Update.exe
+)
 @rem Setup the command line
-
 set CLASSPATH=MTE-Updater.jar
 
 @rem Execute MTEUpdater
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %MTE_UPDATER_OPTS%  -jar "%CLASSPATH%" %CMD_LINE_ARGS%
+
+Pause
 
 :end
 @rem End local scope for the variables with windows NT shell

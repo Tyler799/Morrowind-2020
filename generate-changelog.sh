2061e8010efcade9aaa774bbfe74be44458e9f23
yooksi <yooksi@zoho.com>
2019-04-20 22:03:17 +0200

    Fix section name parsing problem
    Sections names need to be encapsulated with single quotes

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -16,8 +16,8 @@ cacheLog="--cache_log /dev/null"
 # Define all labels you want excluded from the changelog:
 excludeLabels="--exclude-labels task, invalid"
 
-bugsLabel="--bugs-label \"**Fixed problems:**\""
-enhancementLabel="-enhancement-label \"**Implemented improvements:**\""
+bugsLabel="--bugs-label '**Fixed problems:**'"
+enhancementLabel="--enhancement-label '**Implemented improvements:**'"
 
 # Issues with the specified labels will be always added to these sections:
 enhancementLabels="--enhancement-labels improvement"
